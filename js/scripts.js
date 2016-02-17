54b118a5f499e198566ae5eeea758a0e1ef497c2
Scott <scottmcintire90@gmail.com>
2016-02-17 08:22:53 -0800

    updated js code for option selector: budget

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,19 @@ $(document).ready(function() {
   $("form#destination").submit(function(event) {
     var name = $("input#name").val();
     var age = parseInt($("input#age").val());
+    var budget = $('#budget option:selected').val();
 
     if (age >= 30) {
-    vacation = "Hawaii";
-    $("#vacationDestination").empty().append(vacation);
-  }
+      $("#vacationDestination").empty().append("Hawaii");
+
+  } else if (budget == "cheap") {
+    debugger;
+      $("#vacationDestination").empty().append("Thailand");
+
+  } else {
+
+      $("#vacationDestination").empty().append("Portland");
+    }
 
     $("#userName").empty().append(name);
     $("#answer").show();
