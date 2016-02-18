a5939ed22c5611cd9e50067610b4fec8929ce783
Scott <scottmcintire90@gmail.com>
2016-02-17 18:33:06 -0800

    updated jquery results to answers from the form

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,32 @@ $(document).ready(function() {
     var name = $("input#name").val();
     var age = parseInt($("input#age").val());
     var budget = $('#budget option:selected').val();
+    var climate = $('#climate option:selected').val();
+    var distance = $('#distance option:selected').val();
 
-    if (age >= 30) {
-      $("#vacationDestination").empty().append("Hawaii");
+    if (age >= 40 && climate === 'hot') {
+      $("#vacationDestination").empty().append('go swimming in Hawaii');
 
-  } else if (budget == "cheap") {
-    debugger;
-      $("#vacationDestination").empty().append("Thailand");
+  } else if (age >=40 && climate === 'cold') {
+      $("#vacationDestination").empty().append('Iceland');
+
+  } else if (age <= 10) {
+      $("#vacationDestination").empty().append('Chuck E Cheese');
+
+  } else if (age >10 && age < 20 && distance === 'closeBy') {
+      $("#vacationDestination").empty().append('Palm Springs');
+
+  } else if (age < 40 && budget === 'expensive') {
+      $("#vacationDestination").empty().append('New York City');
+
+  } else if (age < 40 && distance === 'farAway') {
+      $("#vacationDestination").empty().append('Thailand');
+
+  } else if (age < 40) {
+      $("#vacationDestination").empty().append('Las Vegas');
 
   } else {
-
-      $("#vacationDestination").empty().append("Portland");
+      $("#vacationDestination").empty().append('Portland');
     }
 
     $("#userName").empty().append(name);
