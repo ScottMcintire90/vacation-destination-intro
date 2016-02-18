70ba131440f269f8bdb15f6af91a0138cfd57f43
Scott <scottmcintire90@gmail.com>
2016-02-17 20:01:14 -0800

    added images to vacation destination result

--- js/scripts.js
+++ js/scripts.js
@@ -7,28 +7,27 @@ $(document).ready(function() {
     var distance = $('#distance option:selected').val();
 
     if (age >= 40 && climate === 'hot') {
-      $("#vacationDestination").empty().append('go swimming in Hawaii');
+      $("#vacationDestination").empty().append('go swimming in Hawaii','<br>','<img src="img/hawaii.jpg" style="width:625px;height:417px">');
 
   } else if (age >=40 && climate === 'cold') {
-      $("#vacationDestination").empty().append('flew over the arctic to Iceland');
+      $("#vacationDestination").empty().append('flew over to Iceland','<br>','<img src="img/iceland.jpg">');
 
   } else if (age <= 10) {
-      $("#vacationDestination").empty().append('played some video games at Chuck E Cheese');
+      $("#vacationDestination").empty().append('played some video games at Chuck E Cheese','<br>','<img src="img/chuck.jpg" style="width:625px;height:417px">');
+
+  } else if (age > 30 && age < 40) {
+          $("#vacationDestination").empty().append('gambled away your life savings in Las Vegas','<br>','<img src="img/vegas.jpg" style="width:625px;height:417px">')
 
   } else if (age >10 && age < 20 && distance === 'closeBy') {
-      $("#vacationDestination").empty().append('soaked up some sun in Palm Springs');
+      $("#vacationDestination").empty().append('soaked up some sun in Palm Springs','<br>','<img src="img/palm.jpg" style="width:625px;height:417px">');
 
   } else if (age < 40 && budget === 'expensive') {
-      $("#vacationDestination").empty().append('saw a broadway play in New York City');
+      $("#vacationDestination").empty().append('saw a broadway play in New York City','<br>','<img src="img/nyc.jpg" style="width:625px;height:417px">');
 
   } else if (age < 40 && distance === 'farAway') {
-      $("#vacationDestination").empty().append('relaxed on a beach in Thailand');
-
-  } else if (age < 40) {
-      $("#vacationDestination").empty().append('gambled away your life savings in Las Vegas');
-
+      $("#vacationDestination").empty().append('relaxed on a beach in Thailand','<br>','<img src="img/thailand.jpg" style="width:625px;height:417px">');
   } else {
-      $("#vacationDestination").empty().append('got a Voodoo donut in Portland');
+      $("#vacationDestination").empty().append('got a Voodoo donut in Portland','<br>','<img src="img/portland.jpg" style="width:625px;height:417px">');
     }
 
     $("#userName").empty().append(name);
